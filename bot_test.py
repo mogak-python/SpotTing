6f9cc41c34425f43c57909c24f7532b7b86085af
hhhyeong <yommyhy77@gmail.com>
2019-07-13 14:44:37 +0900

    modify config

--- bot_test.py
+++ bot_test.py
@@ -1,6 +1,5 @@
 from slacker import Slacker
 from config import CONFIG_TOKEN
-# slack_token = 'xoxb-565300806020-695469067527-xW9C61S3Mr22TieUdqwNzogX'
 slack_token = CONFIG_TOKEN
 slack = Slacker(slack_token)
 # slack.chat.post_message('#bot_test', '우리지금만나')
