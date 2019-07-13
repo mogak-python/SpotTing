2ec77e9f4c78aa2f8cb76aa4dcb720e1f554e01e
hhhyeong <yommyhy77@gmail.com>
2019-07-13 14:42:04 +0900

    apply .gitignore

--- bot_test.py
+++ bot_test.py
@@ -1,9 +1,8 @@
 from slacker import Slacker
-
+from config import CONFIG_TOKEN
 # slack_token = 'xoxb-565300806020-695469067527-xW9C61S3Mr22TieUdqwNzogX'
-slack_token = 'xoxb-565300806020-695469067527-ZiNFCZUyZondlIsQs6cRze62'
+slack_token = CONFIG_TOKEN
 slack = Slacker(slack_token)
 # slack.chat.post_message('#bot_test', '우리지금만나')
-
 if __name__ == "__main__":
     slack.chat.post_message('#bots_test', "우리지금만나")
