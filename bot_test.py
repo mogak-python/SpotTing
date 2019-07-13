824b3e973607c26a7e62ab3d93250109ecec6c71
hhhyeong <yommyhy77@gmail.com>
2019-07-13 14:09:08 +0900

    third

--- bot_test.py
+++ bot_test.py
@@ -1,6 +1,7 @@
 from slacker import Slacker
 
-slack_token = 'xoxb-565300806020-695469067527-xW9C61S3Mr22TieUdqwNzogX'
+# slack_token = 'xoxb-565300806020-695469067527-xW9C61S3Mr22TieUdqwNzogX'
+slack_token = 'xoxb-565300806020-695469067527-ZiNFCZUyZondlIsQs6cRze62'
 slack = Slacker(slack_token)
 # slack.chat.post_message('#bot_test', '우리지금만나')
 
